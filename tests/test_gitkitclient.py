114c7acd7769afe6b584e9cd63948e6445b55ae8
Victor Menezes <menezes.victor@gmail.com>
2015-09-28 23:00:18 -0400

    fixed assert StopIterator test

--- tests/test_gitkitclient.py
+++ tests/test_gitkitclient.py
@@ -110,7 +110,8 @@ class GitkitClientTestCase(unittest.TestCase):
 
       # Should stop since no more result
       rpc_mock.return_value = {}
-      self.assertRaises(StopIteration, next(iterator))
+      with self.assertRaises(StopIteration):
+        next(iterator)
 
       expected_call = [(('downloadAccount', {'maxResults': 10}),),
                        (('downloadAccount',
