b98b0ed43557675e2d2ccb6cba8b777105bcb52f
Lucas Connors <laconnors@google.com>
2015-09-29 17:26:55 -0700

    Allow tests to be run directly from setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,22 @@
 from setuptools import setup
+import sys
 
 install_requires = [
   'oauth2client>=1.3.2',
   'pyOpenSSL==0.14',
   'simplejson>=2.3.2',
   ]
+tests_require = list(install_requires)
+
+# Python 2 requires Mock to run tests
+if sys.version_info < (3, 0):
+    tests_require += ['pbr==1.6', 'Mock']
 
 packages = ['identitytoolkit',]
 
 setup(
   name = 'identity-toolkit-python-client',
   packages = packages,
-  install_requires = install_requires,
   license="Apache 2.0",
   version = '0.1.6',
   description = 'Google Identity Toolkit python client library',
@@ -20,4 +25,7 @@ setup(
   download_url = 'https://github.com/google/identity-toolkit-python-client/archive/master.zip',
   keywords = ['identity', 'google', 'login', 'toolkit'], # arbitrary keywords
   classifiers = [],
+  install_requires = install_requires,
+  tests_require = tests_require,
+  test_suite = 'tests',
 )
