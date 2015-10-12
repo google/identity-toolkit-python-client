50f5d66f96334a998807e17ce3910c55f34212a1
Lucas Connors <laconnors@google.com>
2015-10-12 14:29:07 -0700

    Added Python 3.3 to Travis-CI

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(
     'License :: OSI Approved :: Apache Software License',
     'Operating System :: OS Independent',
     'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.3',
     'Programming Language :: Python :: 3.4',
     'Topic :: Internet :: WWW/HTTP',
   ],
